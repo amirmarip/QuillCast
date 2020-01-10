951ef0b7d4fc24dcd158b15872e90eb6fb14c971
aucetair <aucetair@gmail.com>
2020-01-10 21:45:11 +0800

    Removed create_database py code

--- create_database.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-def main():
-    conn = sqlite3.connect('alphanum.sqlite')
-    cur = conn.cursor()
-    cur.execute('DROP TABLE IF EXISTS writing')
-    cur.execute('CREATE TABLE writing (letcode INT, lowerCase TEXT, upperCase TEXT, numbers TEXT)')
-    conn.close()
-if __name__ == '__main__': main()
